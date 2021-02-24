1fccf4e8ef7081ebfa04d411b384b3efe8ee9f2b
frostwOw13 <frostwow13@yandex.ru>
2021-02-24 19:34:48 +0300

    feat: finish task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (Array.isArray(array) && array.length) {
+    let min = 0;
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] < min) 
+        min = array[i];
+    }
+    return min;
+  } else {
+    return 0;
+  }
+  
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (Array.isArray(array) && array.length) {
+    let max = 0;
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] > max) 
+        max = array[i];
+    }
+    return max;
+  } else {
+    return 0;
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (Array.isArray(array) && array.length) {
+    let sum = 0;
+    for (let i = 0; i < array.length; i++) {
+      sum += array[i];
+    }
+    const avg = sum / array.length;
+    return avg;
+  } else {
+    return 0;
+  }
 }
